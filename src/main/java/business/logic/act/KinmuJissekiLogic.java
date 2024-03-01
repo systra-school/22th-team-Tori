0b79255613d058ab39d958429facb2dd93842a1c
YuuShiraishi <y.shiraishi.garnet2@gmail.com>
2024-03-01 14:21:14 +0900

    障害表014 2024/03/01 白石  備考：木島さんにバトンタッチ

--- src/main/java/business/logic/act/KinmuJissekiLogic.java
+++ src/main/java/business/logic/act/KinmuJissekiLogic.java
@@ -12,13 +12,12 @@ import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 
-import exception.CommonException;
-
 import business.db.dao.act.KinmuJissekiDao;
 import business.dto.LoginUserDto;
 import business.dto.act.KinmuJissekiDto;
 import business.logic.utils.CheckUtils;
 import business.logic.utils.CommonUtils;
+import exception.CommonException;
 
 /**
  * 説明：ログイン処理のロジック
@@ -204,7 +203,44 @@ public class KinmuJissekiLogic {
             String startTimeShift = kinmuJissekiDto.getStartTimeShift();
             String endTimeShift = kinmuJissekiDto.getEndTimeShift();
             String breakTimeShift = kinmuJissekiDto.getBreakTimeShift();
-
+            
+            //障害表014対応 2024/03/01 白石
+            
+            /*
+             * 計算処理を行うために各時間を
+             * 秒に変換する。
+             */
+            long startTimeShiftLong = CommonUtils.getSecond(startTimeShift);
+            long endTimeShiftLong = CommonUtils.getSecond(endTimeShift);
+            long breakTimeShiftLong = CommonUtils.getSecond(breakTimeShift);
+            
+            // 実労働時間(終了時間 - 開始時間)
+            long jitsudouShiftTimeS = (endTimeLong - startTimeLong - breakTimeLong); // 秒
+            
+            if (jitsudouShiftTimeS < 0) {
+                // 休憩が多かったとき
+            	jitsudouShiftTimeS = 0;
+            }
+            //時間外労働時間(実労働時間 - ())
+            long jikangaiTimeS = jitsudouShiftTimeS - (endTimeShiftLong - startTimeShiftLong - breakTimeShiftLong);
+            
+            if (jitsudouShiftTimeS < 0) {
+            	// 時間外労働が無かった時
+            	jikangaiTimeS = 0;
+            }
+            
+            // 秒を60で除算する → 分に変換。
+            long jikangaiTimeM = jikangaiTimeS / 60; // 分
+            // 分を60で除算する → 時に変換。
+            long jikangaiTimeH = jikangaiTimeM / 60; // 時
+            // 分を60で除算したときの余り → 分を算出する。
+            jikangaiTimeM = jikangaiTimeM % 60; // 余りが分になる
+            
+            StringBuffer jikangaiTime = new StringBuffer();
+            jikangaiTime.append(CommonUtils.padWithZero(String.valueOf(jikangaiTimeH), 2));
+            jikangaiTime.append(colon);
+            jikangaiTime.append(CommonUtils.padWithZero(String.valueOf(jikangaiTimeM), 2));
+            
             if (CheckUtils.isEmpty(startTimeShift) || CheckUtils.isEmpty(endTimeShift) || CheckUtils.isEmpty(breakTimeShift)) {
                 // シフトがない（休日の場合）
                 // 実働時間を勤務実績Dtoの休日へセット
@@ -214,7 +250,7 @@ public class KinmuJissekiLogic {
 
                 // 実働時間を勤務実績Dtoの勤務実績へセット
                 kinmuJissekiDto.setJitsudouTime(jitsudouTime.toString());
-            }
+                kinmuJissekiDto.setJikangaiTime(jikangaiTime.toString());            }
         }
     }
 }
